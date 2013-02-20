1e1eb948b399f220556628ebf7b088fdd5c65ad1
Ole Martin Gjersvik <olem@olem.org>
2013-02-20 21:21:26 +0100

    First resize test.

--- web/main.dart
+++ web/main.dart
@@ -1,4 +1,16 @@
+import 'dart:html';
+
+import 'package:game_loop/game_loop.dart';
+
+resize(game){
+  print('resize');
+}
+
 void main() {
+  var gameLoop = new GameLoop(query('#htmlblocks'));
   
+  gameLoop.onResize = resize;
+  
+  gameLoop.start();
 }
 
