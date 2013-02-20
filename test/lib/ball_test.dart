1d2383ce00b83d1ae482b2b7b84ab8915d9ead77
Ole Martin Gjersvik <olem@olem.org>
2013-02-20 20:39:31 +0100

    FAIL: Ball: new Ball(10,5) returns a ball object with y:5

--- test/lib/ball_test.dart
+++ test/lib/ball_test.dart
@@ -9,6 +9,10 @@ main(){
       var ball = new Ball(10,5);
       expect(ball.x, 10);
     });
+    test('new Ball(10,5) returns a ball object with y:5',() {
+      var ball = new Ball(10,5);
+      expect(ball.y, 5);
+    });
   });
 }
 
