67c67116a98605f0efa82eea363c8a9425f9c337
Ole Martin Gjersvik <olem@olem.org>
2013-02-25 18:49:49 +0100

    3 new test passing
    PASS: MouseData: Have a property x
    PASS: MouseData: Clear sets x to zero
    PASS: MouseData: Make copy x

--- lib/mouse_data.dart
+++ lib/mouse_data.dart
@@ -3,6 +3,22 @@ library mouse_data;
 import 'input_data.dart';
 
 class MouseData extends InputData {
+  num x = 0;
   
   MouseData():super();
+  
+  void clear() {
+    x = 0;
+    return super.clear();
+  }
+  
+  MouseData make ([MouseData recycle]) {
+    if(!?recycle){
+      recycle = new MouseData();
+    }
+    recycle = super.make(recycle);
+    
+    recycle.x = this.x;
+    return recycle;
+  }
 }
