daafae0b4dcebbca7b658a0a1c4abffef12f79bf
Ole Martin Gjersvik <olem@olem.org>
2013-02-26 22:59:10 +0100

    Inmplemeted InputSensor
    PASS: InputSensor: getNewData return a new instanse of InputData
    PASS: InputSensor: have a property data for active InputData
    PASS: InputSensor: newVersion sets a new instanase as data and returns
    the old one;
    PASS: InputSensor: you can recycle old InputData for reuse

--- lib/input_sensor.dart
+++ lib/input_sensor.dart
@@ -4,18 +4,26 @@ import 'input_data.dart';
 
 class InputSensor {
   List _recycle = new List();
+  InputData data;
   
-  InputSensor();
+  InputSensor(){
+    data = getNewData();
+  }
   
   InputData newVertion(){
+    var olddata = data;
+    var newdata;
+    if(!_recycle.isEmpty){
+      newdata = _recycle.removeLast();
+    } else {
+      newdata = getNewData();
+    }
     
+    data = olddata.make(newdata);
+    return olddata;
   }
   
-  void recycle(InputData old){
-    
-  }
+  void recycle(InputData old) => _recycle.add(old);
   
-  InputData getNewData(){
-    
-  }
+  InputData getNewData() => new InputData();
 }
