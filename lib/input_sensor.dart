11a8702a79acb3c40b897c3b6185cf5c63164779
Ole Martin Gjersvik <olem@olem.org>
2013-02-26 22:25:40 +0100

    Add new class InputSensor

---
+++ lib/input_sensor.dart
@@ -0,0 +1,21 @@
+library input_sensor;
+
+import 'input_data.dart';
+
+class InputSensor {
+  List _recycle = new List();
+  
+  InputSensor();
+  
+  InputData newVertion(){
+    
+  }
+  
+  void recycle(InputData old){
+    
+  }
+  
+  InputData getNewData(){
+    
+  }
+}
