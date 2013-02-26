b56f402e85ced5c2093fb443ba86525f7a635f57
Ole Martin Gjersvik <olem@olem.org>
2013-02-26 22:29:01 +0100

    New test file for class InputSensor

---
+++ test/input_sensor_test.dart
@@ -0,0 +1,10 @@
+library test.input_sensor;
+
+import 'package:unittest/unittest.dart';
+import 'package:gamelib/input_sensor.dart';
+
+main(){
+  group('InputSensor:',() {
+  });
+}
+
--- test/run_cli.dart
+++ test/run_cli.dart
@@ -5,11 +5,14 @@ import 'packages/unittest/unittest.dart';
 
 import 'input_data_test.dart' as InputDataTest;
 import 'mouse_data_test.dart' as MouseDataTest;
+import 'input_sensor_test.dart' as InputSensorTest;
+
 
 main() {
   useVMConfiguration();
   
   InputDataTest.main();
   MouseDataTest.main();
+  InputSensorTest.main();
 }
 
