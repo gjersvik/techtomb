b0048d46ed599e897ed66b1f89f560ce2eacb987
Ole Martin Gjersvik <olem@olem.org>
2013-03-01 11:33:00 +0100

    Implemented State.time
    PASS: State: DateTime time is utc time.

--- lib/loop/state.dart
+++ lib/loop/state.dart
@@ -1,6 +1,13 @@
 library loop_state;
 
 class State{
+  DateTime time;
   
+  State([DateTime time]){
+    if(!?time){
+      time = new DateTime.now();
+    }
+    this.time = time.toUtc();
+  }
 }
 
--- test/loop/state_test.dart
+++ test/loop/state_test.dart
@@ -1,10 +1,14 @@
 library test.loop_state;
 
-import '../packages/unittest/unittest.dart';
+import 'package:unittest/unittest.dart';
 import 'package:gamelib/loop/state.dart';
 
 main(){
   group('State:',() {
-    
+    test('DateTime time is utc time.',() {
+      var time = new DateTime.now();
+      var data = new State(time);
+      expect(data.time, time.toUtc());
+    });
   });
 }
