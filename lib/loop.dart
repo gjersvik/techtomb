5c791afdee6a762a8b6edeb9a9c5d510ee6dca83
Ole Martin Gjersvik <olem@olem.org>
2013-03-04 23:05:32 +0100

    Loop: set up streams
    PASS: Loop: onBefore is a broadcast Stream
    PASS: Loop: onLoop is a broadcast Stream
    PASS: Loop: onAfter is a broadcast Stream

--- lib/loop.dart
+++ lib/loop.dart
@@ -1,5 +1,22 @@
 library loop;
 
+import "dart:async";
+
+import 'loop/state.dart';
+export 'loop/state.dart';
+
 class Loop {
+  var _onBefore;
+  var _onLoop;
+  var _onAfter;
   
+  Loop(){
+    _onBefore = new StreamController<State>.broadcast();
+    _onLoop = new StreamController<State>.broadcast();
+    _onAfter = new StreamController<State>.broadcast();
+  }
+  
+  Stream<State> get onBefore => _onBefore.stream;
+  Stream<State> get onLoop => _onLoop.stream;
+  Stream<State> get onAfter => _onAfter.stream;
 }
--- test/loop_test.dart
+++ test/loop_test.dart
@@ -5,8 +5,17 @@ import 'package:gamelib/loop.dart';
 
 main(){
   group('Loop:',() {
-    test('',() {
-      
+    test('onBefore is a broadcast Stream',() {
+      var loop = new Loop();
+      expect(loop.onBefore.isBroadcast, isTrue);
+    });
+    test('onLoop is a broadcast Stream',() {
+      var loop = new Loop();
+      expect(loop.onLoop.isBroadcast, isTrue);
+    });
+    test('onAfter is a broadcast Stream',() {
+      var loop = new Loop();
+      expect(loop.onAfter.isBroadcast, isTrue);
     });
   });
 }
