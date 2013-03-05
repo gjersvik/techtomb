ef1bd5e50e5952f8497074831f42a7fc93f5edfd
Ole Martin Gjersvik <olem@olem.org>
2013-03-05 18:22:30 +0100

    Written desigen for loop class

--- test/loop_test.dart
+++ test/loop_test.dart
@@ -8,7 +8,14 @@ class MockRunner extends Mock implements Runner{}
 
 main(){
   group('Loop:',() {
-    test('',() {
+    test('Loop.start() calls Runner.start() wit a tic function',() {
+      fail('Test not implemented');
+    });
+    test('Loop.end() calls Runner.end()',() {
+      fail('Test not implemented');
+    });
+    test('When the tic function is called all the callbacks gets runned',() {
+      fail('Test not implemented');
     });
   });
 }
