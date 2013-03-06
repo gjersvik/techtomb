ef83983556feadac5a4556631470c3e3e833fa7e
Ole Martin Gjersvik <olem@olem.org>
2013-03-06 11:48:28 +0100

    setup canvas

--- web/main.dart
+++ web/main.dart
@@ -5,9 +5,31 @@ import 'package:gamelib/loop.dart';
 import 'package:gamelib/loop/timer_runner.dart';
 import 'package:gamelib/loop/animation_runner.dart';
 
+CanvasRenderingContext2D setupCanvas(){
+  var size = min(window.innerHeight,window.innerWidth);
+  CanvasElement c = query('#htmlblocks');
+  CanvasRenderingContext2D paint = c.getContext('2d');
+  
+  c.height = size;
+  c.width = size;
+  
+  paint.scale(size / 100, size / 100);
+  
+  return paint;
+}
+
 void main() {
   var gameloop = new Loop(new TimerRunner());
   var renderloop = new Loop(new AnimationRunner());
+  var paint = setupCanvas();
+  
+  renderloop.callbacks.add((){
+    paint.clearRect(0, 0, 100, 100);
+    
+    paint.strokeStyle = 'yellow';
+    
+    paint.strokeRect(1, 1, 98, 98, 2);
+  });
   
   gameloop.start();
   renderloop.start();
