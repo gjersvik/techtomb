8f5d611548bf715b3fa33f26daa0ec626e577944
Ole Martin Gjersvik <olem@olem.org>
2013-03-07 23:50:58 +0100

    Refactored some name.

--- web/main.dart
+++ web/main.dart
@@ -22,44 +22,44 @@ CanvasRenderingContext2D setupCanvas(){
 
 World setupBox2d(){
   var world = new World(new Vector(0, 0), true, new DefaultWorldPool());
-  var wallfix = new FixtureDef();
-  wallfix.density = 1;
-  wallfix.restitution = 1;
-  wallfix.friction = 0;
-  wallfix.shape = new PolygonShape();
-  wallfix.shape.setAsBox(60,5);
+  var wall_fix = new FixtureDef();
+  wall_fix.density = 1;
+  wall_fix.restitution = 1;
+  wall_fix.friction = 0;
+  wall_fix.shape = new PolygonShape();
+  wall_fix.shape.setAsBox(60,5);
   
-  var wallbody = new BodyDef();
-  wallbody.type = BodyType.STATIC;
-  wallbody.position = new Vector(50, -5);
+  var wall_body = new BodyDef();
+  wall_body.type = BodyType.STATIC;
+  wall_body.position = new Vector(50, -5);
   
-  world.createBody(wallbody).createFixture(wallfix);
-  wallbody.position = new Vector(50, 105);
-  world.createBody(wallbody).createFixture(wallfix);
+  world.createBody(wall_body).createFixture(wall_fix);
+  wall_body.position = new Vector(50, 105);
+  world.createBody(wall_body).createFixture(wall_fix);
   
-  wallfix.shape.setAsBox(5,60);
-  wallbody.position = new Vector(-5, 50);
-  world.createBody(wallbody).createFixture(wallfix);
-  wallbody.position = new Vector(105, 50);
-  world.createBody(wallbody).createFixture(wallfix);
+  wall_fix.shape.setAsBox(5,60);
+  wall_body.position = new Vector(-5, 50);
+  world.createBody(wall_body).createFixture(wall_fix);
+  wall_body.position = new Vector(105, 50);
+  world.createBody(wall_body).createFixture(wall_fix);
   
   return world;
 }
 
 Body makeBall(world) {
-  var ballfix = new FixtureDef();
-  ballfix.density = 1;
-  ballfix.restitution = 1;
-  ballfix.friction = 0;
-  ballfix.shape = new CircleShape();
-  ballfix.shape.radius = 1;
+  var ball_fix = new FixtureDef();
+  ball_fix.density = 1;
+  ball_fix.restitution = 1;
+  ball_fix.friction = 0;
+  ball_fix.shape = new CircleShape();
+  ball_fix.shape.radius = 1;
   
-  var ballbody = new BodyDef();
-  ballbody.type = BodyType.DYNAMIC;
-  ballbody.position = new Vector(50, 50);
+  var ball_body = new BodyDef();
+  ball_body.type = BodyType.DYNAMIC;
+  ball_body.position = new Vector(50, 50);
   
-  var ball = world.createBody(ballbody);
-  ball.createFixture(ballfix);
+  var ball = world.createBody(ball_body);
+  ball.createFixture(ball_fix);
   
   ball.linearVelocity = new Vector(40, 30);
   
@@ -67,17 +67,17 @@ Body makeBall(world) {
 }
 
 void main() {
-  var gameloop = new Loop(new TimerRunner());
-  var renderloop = new Loop(new AnimationRunner());
+  var game_loop = new Loop(new TimerRunner());
+  var render_loop = new Loop(new AnimationRunner());
   var paint = setupCanvas();
   var world = setupBox2d();
   var ball = makeBall(world);
   
-  gameloop.callbacks.add((){
+  game_loop.callbacks.add((){
     world.step(0.016 , 10, 10);
   });
   
-  renderloop.callbacks.add((){
+  render_loop.callbacks.add((){
     paint.clearRect(0, 0, 100, 100);
     
     paint.fillStyle = 'yellow';
@@ -88,7 +88,7 @@ void main() {
     paint.fill();
   });
   
-  gameloop.start();
-  renderloop.start();
+  game_loop.start();
+  render_loop.start();
 }
 
