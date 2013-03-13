1af3fe3752e6f5b6c8ba8105b8db825ce1cd510d
Ole Martin Gjersvik <olem@olem.org>
2013-03-13 12:01:50 +0100

    Rewrote Loop test

--- test/loop_test.dart
+++ test/loop_test.dart
@@ -6,38 +6,52 @@ import 'package:gamelib/loop.dart';
 
 class MockRunner extends Mock implements Runner{}
 
+class LoopTestData{
+  Loop loop;
+  MockRunner mock;
+  Function _tic = null;
+  
+  LoopTestData() {
+    mock = new MockRunner();
+    loop = new Loop(mock);
+  }
+  
+  tic() {
+    if(_tic == null) {
+      _tic = mock.getLogs(callsTo('start')).last.args[0];
+    }
+    _tic();
+  }
+}
+
 main(){
   group('Loop:',() {
     test('Loop.start() calls Runner.start() wit a tic function',() {
-      var mock = new MockRunner();
-      var loop = new Loop(mock);
-      loop.start();
+      var test = new LoopTestData();
+      test.loop.start();
       
-      mock.getLogs(callsTo('start', new isInstanceOf<Function>())).verify(happenedOnce);
+      test.mock.getLogs(callsTo('start', new isInstanceOf<Function>())).verify(happenedOnce);
     });
     test('Loop.end() calls Runner.stop()',() {
-      var mock = new MockRunner();
-      var loop = new Loop(mock);
-      loop.end();
+      var test = new LoopTestData();
+      test.loop.end();
       
-      mock.getLogs(callsTo('stop')).verify(happenedOnce);
+      test.mock.getLogs(callsTo('stop')).verify(happenedOnce);
     });
     test('When the tic function is called all the callbacks gets runned',() {
-      var mock = new MockRunner();
-      var loop = new Loop(mock);
+      var test = new LoopTestData();
       var list = new List();
       
       createCallback(num) {
         return (_) => list.add(num);
       }
       
-      loop.callbacks.add(createCallback(1));
-      loop.callbacks.add(createCallback(2));
-      loop.callbacks.add(createCallback(3));
+      test.loop.callbacks.add(createCallback(1));
+      test.loop.callbacks.add(createCallback(2));
+      test.loop.callbacks.add(createCallback(3));
       
-      loop.start();
-      var tic = mock.getLogs(callsTo('start')).first.args[0];
-      tic();
+      test.loop.start();
+      test.tic();
       
       expect(list,[1,2,3]);
     });
