8267e85fdad1e82e39e15451ce2c5f09dbbb77e7
Ole Martin Gjersvik <olem@olem.org>
2013-03-14 18:04:08 +0100

    Moved code form main to game.

--- web/game.dart
+++ web/game.dart
@@ -1,19 +1,42 @@
 library game;
+import 'ball.dart';
 
+import 'dart:math' as Math;
 import 'package:box2d/box2d.dart';
 
 import 'package:gamelib/loop.dart';
 import 'package:gamelib/loop/timer_runner.dart';
 
+typedef void TakeState(num gametime, Map<String, dynamic> state);
+
 class Game{
   World world;
+  Loop loop;
+  TakeState _takeState;
   
-  Game(){
-    world = new World(new vec2(0, 0), true, new DefaultWorldPool());
-    
+  Game(): 
+    world = new World(new vec2(0, 0), true, new DefaultWorldPool()),
+    loop = new Loop(new TimerRunner()){
     _createWall();
     
+    var balls = new Balls(world);
+    balls.genBall();
+    balls.genBall();
+    balls.genBall();
+    balls.genBall();
+    balls.genBall();
+    balls.genBall();
+    
+    loop.callbacks.add((_) => world.step(0.016 , 10, 10));
+    
+    loop.callbacks.add((e) => e['gamestate'] = balls.toGameState());
+    
+    loop.callbacks.add((event) => _takeState(event['gametime'],event['gamestate']));
   }
+  
+  start() => loop.start();
+  
+  postState(TakeState callback) => _takeState = callback;
   
   _createWall(){
     var shape = new PolygonShape();
@@ -33,4 +56,31 @@ class Game{
     body.position = new vec2(110, 50);
     world.createBody(body).createFixtureFromShape(shape);
   }
+}
+
+class Balls{
+  List balls;
+  World _world;
+  Math.Random _rand;
+  
+  Balls(this._world): _rand = new Math.Random(), balls = [];
+  
+  void genBall(){
+    var ball = new BallBody(_world);
+    ball.x = _rand.nextDouble() * 100;
+    ball.y = _rand.nextDouble() * 100;;
+    ball.angle = (_rand.nextDouble() * Math.PI *2) - Math.PI;
+    balls.add(ball);
+  }
+  
+  Map toGameState(){
+    var i = 1;
+    var map = {};
+    balls.forEach((ball) {
+      map['Ball$i'] = ball.toGameState();
+      i += 1;
+    });
+    return map;
+  }
+  
 }
--- web/main.dart
+++ web/main.dart
@@ -1,62 +1,13 @@
-import 'ball.dart';
 import 'render.dart';
 import 'game.dart';
-
-import 'dart:math' as Math;
-
-import 'package:box2d/box2d.dart';
-
-import 'package:gamelib/loop.dart';
-import 'package:gamelib/loop/timer_runner.dart';
-
-class Balls{
-  List balls;
-  World _world;
-  Math.Random _rand;
-  
-  Balls(this._world): _rand = new Math.Random(), balls = [];
-  
-  void genBall(){
-    var ball = new BallBody(_world);
-    ball.x = _rand.nextDouble() * 100;
-    ball.y = _rand.nextDouble() * 100;;
-    ball.angle = (_rand.nextDouble() * Math.PI *2) - Math.PI;
-    balls.add(ball);
-  }
-  
-  Map toGameState(){
-    var i = 1;
-    var map = {};
-    balls.forEach((ball) {
-      map['Ball$i'] = ball.toGameState();
-      i += 1;
-    });
-    return map;
-  }
-  
-}
 
 void main() {
   var render = new Render('#htmlblocks');
   var game = new Game();
   
-  var game_loop = new Loop(new TimerRunner());
+  game.postState(render.addState);
   
-  var balls = new Balls(game.world);
-  balls.genBall();
-  balls.genBall();
-  balls.genBall();
-  balls.genBall();
-  balls.genBall();
-  balls.genBall();
-  
-  game_loop.callbacks.add((_) => game.world.step(0.016 , 10, 10));
-  
-  game_loop.callbacks.add((e) => e['gamestate'] = balls.toGameState());
-  
-  game_loop.callbacks.add((Map event) => render.addState(event['gametime'],event['gamestate']));
-  
-  game_loop.start();
+  game.start();
   render.start();
 }
 
