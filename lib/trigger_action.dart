f560b390bb9d38385b4e536087006b50d100ba0c
Ole Martin Gjersvik <olem@olem.org>
2013-03-14 11:02:39 +0100

    Added class TriggerAction

---
+++ lib/trigger_action.dart
@@ -0,0 +1,26 @@
+library trigger_action;
+
+class TriggerAction{
+  final String name;
+  bool _triggerd = false;
+  
+  TriggerAction(this.name);
+  
+  bool get triggerd => _triggerd;
+  
+  Map<string,dynamic> get data{
+    var data = new Map();
+    if(_triggerd == true){
+      data['name'] == name;
+    }
+    return data;
+  }
+  void set data(Map<string,dynamic> data){
+    if(data['name'] == name){
+      trigger; 
+    }
+  }
+  
+  void trigger() => _triggerd = true;
+  void reset() => _triggerd = false;
+}
