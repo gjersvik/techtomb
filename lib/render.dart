11fe2d1d6e910983827024c0eac1077dfed0a566
Ole Martin Gjersvik <olem@olem.org>
2013-03-15 16:33:46 +0100

    Refactored render code to one library.

---
+++ lib/render.dart
@@ -0,0 +1,8 @@
+library gamelib_render;
+
+import 'share.dart';
+export 'share.dart';
+
+import 'dart:html';
+
+part 'src/render/animation_runner.dart';
--- lib/src/render/animation_runner.dart
+++ lib/src/render/animation_runner.dart
@@ -1,21 +1,15 @@
-library animation_runner;
-
-import 'dart:html';
-
-import 'runner.dart';
-export 'runner.dart';
-
+part of gamelib_render;
 
 class AnimationRunner extends Runner {
   var _tic = null;
-  
+
   _callback( _ ) {
     if(_tic != null){
       _tic();
       window.requestAnimationFrame(_callback);
     }
   }
-  
+
   start( Function tic) {
     _tic = tic;
     window.requestAnimationFrame(_callback);
