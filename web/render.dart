76858fe5a78ccc437a42e788745087c141c3e03b
Ole Martin Gjersvik <olem@olem.org>
2013-03-15 10:55:18 +0100

    Render can now paint pads.

--- web/render.dart
+++ web/render.dart
@@ -10,41 +10,53 @@ class Render{
   CanvasElement element;
   CanvasRenderingContext2D context;
   Loop loop;
-  
+
   Map _state = {};
-  
+
   Render(tag):
     loop = new Loop(new AnimationRunner()){
     var size = min(window.innerHeight,window.innerWidth);
     element = query(tag);
     context = element.getContext('2d');
-    
+
     element.height = size;
     element.width = size;
-    
+
     context.scale(size / 100, size / 100);
-    
+
     loop.callbacks.add(_paint);
   }
-  
+
   start() => loop.start();
-  
+
   addState(num gametime, Map state) => _state = state;
-  
+
   void _paint(_){
     context.clearRect(0, 0, 100, 100);
-    
+
     _state.forEach((key, value){
       if(value['type'] == 'ball'){
         _paintBall(value);
       }
+      if(value['type'] == 'pad'){
+        _paintPad(value);
+      }
     });
   }
-  
+
   void _paintBall(Map ball){
     context.fillStyle = 'yellow';
     context.beginPath();
     context.arc(ball['x'], ball['y'], ball['size'], 0, PI*2, false);
     context.fill();
+  }
+
+  void _paintPad(Map pad){
+    context.fillStyle = 'red';
+    context.fillRect(
+        pad['x'] - pad['width'] / 2,
+        pad['y'] - pad['height'] / 2,
+        pad['width'],
+        pad['height']);
   }
 }
