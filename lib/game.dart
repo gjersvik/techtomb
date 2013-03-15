c0ed73eef8b5c1c928f23dd51b79562169aa9aa5
Ole Martin Gjersvik <olem@olem.org>
2013-03-15 18:26:08 +0100

    Added new class GameObjects to game

--- lib/game.dart
+++ lib/game.dart
@@ -8,6 +8,7 @@ import 'dart:async';
 part 'src/game/timer_runner.dart';
 
 part 'src/game/game_object.dart';
+part 'src/game/game_objects.dart';
 
 part 'src/game/trigger_action.dart';
 part 'src/game/analog_action.dart';
---
+++ lib/src/game/game_objects.dart
@@ -0,0 +1,17 @@
+part of gamelib_game;
+
+class GameObjects{
+  Map<String, GameObject> objects = {};
+
+  GameObject get(String name) => objects[name];
+  GameObject add(GameObject o) => objects[o.name] = o;
+  GameObject remove(String name) => objects.remove(name);
+  GameObject removeObject(GameObject o) => remove(o.name);
+
+  Map<String, dynamic> toGameState() {
+    var state = {};
+    objects.forEach((key, object) => state[key] = object.toGameState());
+    return state;
+  }
+}
+
