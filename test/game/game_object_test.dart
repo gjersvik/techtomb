1a1a0e875502d5c09703a7d47860638e250d7119
Ole Martin Gjersvik <olem@olem.org>
2013-03-22 19:24:26 +0100

    Tests for GameObjects new event code.
    PASS: GameObject: fire test event
    PASS: GameObject: GameEvent has name data and object
    PASS: GameObject: destroy send destroying event
    PASS: GameObject: destroy closes stream

--- test/game/game_object_test.dart
+++ test/game/game_object_test.dart
@@ -17,5 +17,35 @@ main(){
       var object = new GameObject('TestName','TestType');
       expect(object.toGameState(), {'type':'TestType'});
     });
+    test('fire test event',() {
+      var object = new GameObject('TestName','TestType');
+      var listen = false;
+      object.on.listen((_)=> listen = true);
+      object.fire('testevent');
+      expect(listen, isTrue);
+    });
+    test('GameEvent has name data and object',() {
+      var object = new GameObject('TestName','TestType');
+      GameEvent event;
+      object.on.listen((e)=> event = e);
+      object.fire('testevent','testdata');
+      expect(event.name, 'testevent');
+      expect(event.data, 'testdata');
+      expect(event.object, object);
+    });
+    test('destroy send destroying event',() {
+      var object = new GameObject('TestName','TestType');
+      GameEvent event;
+      object.on.listen((e)=> event = e);
+      object.destroy();
+      expect(event.name, 'destroying');
+    });
+    test('destroy closes stream',() {
+      var object = new GameObject('TestName','TestType');
+      var done = false;
+      object.on.listen(null,onDone: ()=> done = true);
+      object.destroy();
+      expect(done, isTrue);
+    });
   });
 }
