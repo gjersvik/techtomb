64edc8e07b7487c3fe94625b7568cde4802e5c87
Ole Martin Gjersvik <olem@olem.org>
2013-03-22 18:54:12 +0100

    Game now suport events.

--- lib/src/game/game_object.dart
+++ lib/src/game/game_object.dart
@@ -2,8 +2,31 @@ part of gamelib_game;
 
 class GameObject extends NamedObject{
   final String type;
+  EventSink<GameEvent> _sink;
+  Stream<GameEvent> on;
 
-  GameObject(name,this.type): super(name);
+  GameObject(name,this.type): super(name){
+    var ctrl = new StreamController.broadcast();
+    _sink = ctrl.sink;
+    on = ctrl.stream;
+  }
 
   Map<String, dynamic> toGameState() => {'type' :type};
+
+  void fire(name,[data]){
+    _sink.add(new GameEvent(name, this, data));
+  }
+
+  void destroy(){
+    fire('destroying');
+    _sink.close();
+  }
+}
+
+class GameEvent{
+  final String name;
+  final GameObject object;
+  var data = null;
+
+  GameEvent(this.name, this.object, [this.data]);
 }
--- lib/src/game/game_objects.dart
+++ lib/src/game/game_objects.dart
@@ -1,6 +1,30 @@
 part of gamelib_game;
 
-class GameObjects extends NamedObjects<GameObject>{
+class GameObjects<T extends GameObject> extends NamedObjects<T>{
+  Stream<GameEvent> on;
+  EventSink _sink;
+  Map _subs;
+
+  GameObjects():_subs = {}, super(){
+    var ctrl = new StreamController.broadcast();
+    _sink = ctrl.sink;
+    on = ctrl.stream;
+    on.listen((e){
+      if(e.name == 'destroying'){
+        removeObject(e.object);
+      }
+    });
+  }
+
+  T add(T o){
+    _subs[o.name] = o.on.listen(_sink.add);
+    return super.add(o);
+  }
+  T remove(String name){
+    _subs.remove(name).cancel();
+    return super.remove(name);
+  }
+
   Map<String, Map<String,dynamic>> toGameState() {
     var state = {};
     objects.forEach((key, object) => state[key] = object.toGameState());
