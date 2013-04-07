6ecf39704c27891201f9cd5cae5a3fc3e542e585
Ole Martin Gjersvik <olem@olem.org>
2013-04-07 13:42:02 +0200

    /build.dart successfully redirects to /tools/bin/build.dart

--- build.dart
+++ build.dart
@@ -1,3 +1,21 @@
-import 'tools/bin/build.dart' as build;
+import 'dart:io';
+//import 'tools/bin/build.dart' as build;
 
-main() => build.main();
+main(){
+  var options = new Options();
+  var args  = [];
+  args.add('tools/bin/build.dart');
+  args.addAll(options.arguments);
+
+  var prooptions = new ProcessOptions();
+  prooptions.workingDirectory = new Directory.current().path;
+
+  Process.start(options.executable, args).then(setupProcess);
+}
+
+setupProcess(Process pro){
+  pro.stdin.writeStream(stdin);
+  stderr.writeStream(pro.stderr);
+  stdout.writeStream(pro.stdout);
+  pro.exitCode.then((code)=> exit(code));
+}
--- tools/bin/build.dart
+++ tools/bin/build.dart
@@ -1,7 +1,15 @@
 library build;
+
+import 'dart:io';
 import 'package:dart_config/default_server.dart';
 
 void main(){
+  var options = new Options();
+  print(options.arguments);
+  print(options.executable);
+  print(options.script);
+  print(new Directory.current().path);
+
   loadConfig().then((conf) {
     print(conf['dartsdk']);
   });
