9132aa45d43e59ebf15b8105be0bf67b51473a35
Ole Martin Gjersvik <olem@olem.org>
2013-04-11 12:50:04 +0200

    Rewritten build.dart to make sure that tools can run.

--- build.dart
+++ build.dart
@@ -1,21 +1,47 @@
 import 'dart:io';
-//import 'tools/bin/build.dart' as build;
+import 'dart:async';
 
-main(){
-  var options = new Options();
-  var args  = [];
-  args.add('tools/bin/build.dart');
-  args.addAll(options.arguments);
+Path dart_sdk = new Path(new Options().executable).directoryPath;
 
-  var prooptions = new ProcessOptions();
-  prooptions.workingDirectory = new Directory.current().path;
+Future<int> run(exec, args, working_dir){
+  var completer = new Completer();
 
-  Process.start(options.executable, args, prooptions).then(setupProcess);
+  var options = new ProcessOptions();
+  options.workingDirectory = working_dir;
+
+  exec = dart_sdk.append(exec).toNativePath();
+
+  Process.start(exec, args, options).then((Process pro){
+    pro.stdin.writeStream(stdin);
+    stderr.writeStream(pro.stderr);
+    stdout.writeStream(pro.stdout);
+    pro.exitCode.then((code) => completer.complete(code));
+  });
+
+  return completer.future;
 }
 
-setupProcess(Process pro){
-  pro.stdin.writeStream(stdin);
-  stderr.writeStream(pro.stderr);
-  stdout.writeStream(pro.stdout);
-  pro.exitCode.then((code)=> exit(code));
+Future pub(){
+  var completer = new Completer();
+  var dir = new Directory('tools/packages');
+  dir.exists().then((exits) {
+    if(exits){
+      completer.complete();
+    } else {
+      run('pub.bat',['install'],new Directory('tools').path).then((code) => code == 0 ? completer.complete() : completer.completeError('pub error'));
+    }
+  });
+
+  return completer.future;
+}
+
+void tools(_){
+  var args  = ['--checked','tools/bin/build.dart'];
+  args.addAll(new Options().arguments);
+
+  run('dart', args, new Directory.current().path).then((code)=>exit(code));
+}
+
+main(){
+  pub().then(tools);
 }
