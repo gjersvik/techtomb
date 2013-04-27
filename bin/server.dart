eb2b3dd92c788a494018f1b8464fc7aabc23d406
Ole Martin Gjersvik <olem@olem.org>
2013-04-27 11:58:29 +0200

    Server send test message.

--- bin/server.dart
+++ bin/server.dart
@@ -1,22 +1,10 @@
 import 'dart:io';
+import 'package:fukiya/fukiya.dart';
 
 main(){
-  dartHandler(HttpRequest request) {
-    print('New request');
-    request.response.write('Dart is optionally typed');
-    request.response.close();
-  };
-
-  HttpServer.bind('127.0.0.1', 8888).then((HttpServer server) {
-    server.listen((request) {
-      print("got request");
-      if (request.uri.path == '/languages/dart') {
-        dartHandler(request);
-      } else {
-        request.response.write('Not found');
-        request.response.statusCode = HttpStatus.NOT_FOUND;
-        request.response.close();
-      }
-    });
+  var midleware = new Fukiya();
+  midleware.get('/', (FukiyaContext context) {
+    context.send('Testing');
   });
+  midleware.listen('0.0.0.0', 8080);
 }
